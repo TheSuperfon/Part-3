a8adc3976034d128c1b62340f8e94247818f9013
auburn <astefanadon@gmail.com>
2024-03-14 14:26:16 -0400

    Week 9 task 1a (merchant inheritance) and task 1b (archer inheritance and attack overides and prefab spawn)

---
+++ Assets/Week 9/Scripts/Archer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Villager
+{
+    public GameObject ArrowPrefab;
+    public Transform spawnPoint;
+    public float delay = 0.46f;
+    protected override void Attack()
+    {
+        destination = transform.position;
+
+        base.Attack();
+        Invoke("SpawnArrow", delay);
+    }
+
+
+
+    void SpawnArrow()
+    {
+        Instantiate(ArrowPrefab, spawnPoint.position, spawnPoint.rotation);
+
+
+    }
+}
