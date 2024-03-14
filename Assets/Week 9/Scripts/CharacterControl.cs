94023f4939ec70610109ca90cb7dd8ece4eeef98
auburn <astefanadon@gmail.com>
2024-03-14 17:06:42 -0400

    Task 4b completed and gives the name of the characters to the UI

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI VillagerName;
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -16,4 +18,20 @@ public class CharacterControl : MonoBehaviour
         SelectedVillager.Selected(true);
     }
     
+    protected void Update()
+    {
+
+        
+        if (SelectedVillager == null)
+        {
+            VillagerName.text = ("None Selected");
+        }
+        else
+        {
+            VillagerName.text = SelectedVillager.ToString();
+        }
+    }
+
+
+
 }
