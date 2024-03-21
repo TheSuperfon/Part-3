024449ce6e5a408b421f414387c9b468ac529769
auburn <astefanadon@gmail.com>
2024-03-21 01:28:02 -0400

    Task 4a Dropdown function controls the selection of characters.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,13 +1,21 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI currentSelection;
+    public TMP_Dropdown dropdownmenu;
     public static CharacterControl Instance;
     public static Villager SelectedVillager { get; private set; }
+    public Villager TheThief;
+    public Villager TheArcher;
+    public Villager TheMerchant;
+    public string villagername;
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -16,7 +24,7 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
-        Instance.currentSelection.text = villager.ToString();
+        //Instance.currentSelection.text = villager.ToString();
     }
 
     private void Update()
@@ -29,5 +37,34 @@ public class CharacterControl : MonoBehaviour
     private void Start()
     {
         Instance = this;
+    }
+
+    public void Characterchange(int index)
+    {
+        //Debug.Log(dropdownmenu.options[index].text);
+        villagername = dropdownmenu.options[index].text;
+        
+        if (villagername == ("Thief"))
+        {
+            //Debug.Log(villagername);
+            Instance.currentSelection.text = villagername;
+            SetSelectedVillager(TheThief);
+            
+        }
+        if (villagername == ("Merchant"))
+        {
+            //Debug.Log(villagername);
+            Instance.currentSelection.text = villagername;
+            SetSelectedVillager(TheMerchant);
+        }
+        if (villagername == ("Archer"))
+        {
+            //Debug.Log(villagername);
+            Instance.currentSelection.text = villagername;
+            SetSelectedVillager(TheArcher);
+        }
+
+
+        //CharacterControl.SetSelectedVillager(Thief);
     }
 }
