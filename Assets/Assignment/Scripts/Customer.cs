f1dd81f6179118f6fa6e17bf437c0370432aef98
TheSuperfon <astefanadon@gmail.com>
2024-04-02 21:36:47 -0400

    Customer controller instructs a random customer to approach the bar

--- Assets/Assignment/Scripts/Customer.cs
+++ Assets/Assignment/Scripts/Customer.cs
@@ -11,16 +11,20 @@ public class Customer : MonoBehaviour
     public float Delay;
     public float CustomerWait;
     public float walkspeed;
-    public List<Drinks> drinks;
+    public bool ChildChosen;
+    public bool adultChosen;
+
+    public int DrinkChoice;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        
         //walkspeed = 
         Delay = 0.2f;
         //CustomerWait = 
-        StartCoroutine(ToTheBar(Random.Range(0.5f, 3f)));
+        //StartCoroutine(ToTheBar(Random.Range(0.5f, 3f))); 
     }
 
     // Update is called once per frame
@@ -29,48 +33,55 @@ public class Customer : MonoBehaviour
         
     }
 
-    IEnumerator ToTheBar(float walktime)
+    public void StartBarTrip()
     {
-        yield return new WaitForSeconds(Delay);
+        Debug.Log("ok");
+        CustomerController.choose = false;//since the customer has been chosen stops customercontroller from continuing to choose
+        StartCoroutine(ToTheBar(Random.Range(0.5f, 3f))); //starts the customer going to the bar and randomizes how fast they walk to the bar
+
+    }
+
+
+    public IEnumerator ToTheBar(float walktime)
+    {
+
+        yield return new WaitForSeconds(Delay); //delay a little bit
         float remainingtime = 0;
 
-        while (remainingtime < walktime)
+        while (remainingtime < walktime) //while the remaining time is less than the randomly assigned walktime the code below is ran
         {
-            transform.position = Vector3.Lerp(Starposition.position, Endposition.position, (remainingtime));
+            transform.position = Vector3.Lerp(Starposition.position, Endposition.position, (remainingtime)); //the position of the customer will go from the public and determined start position toward the end position 
             remainingtime += Time.deltaTime;
             yield return null;
         }
 
         yield return new WaitForSeconds(0.5f);
-        StartCoroutine(OrderAtBar(Random.Range(3f, 6f)));
+        StartCoroutine(OrderAtBar(Random.Range(3f, 6f))); //randomizes how long customers will spend ordering
     }
 
-    IEnumerator OrderAtBar(float OrderTime)
+    protected virtual IEnumerator OrderAtBar(float OrderTime)
     {
         yield return new WaitForSeconds(Delay);
-        float remainingtime = 0;
-        SpeechBubble.SetActive(true);
-        yield return new WaitForSeconds(OrderTime);
-        /*while (remainingtime < OrderTime)
+        float remainingtime = 0; //resets remaining time so that customer can get out of here when the time comes
+        SpeechBubble.SetActive(true); //sets the speech bubble active so that they can say order 
+        DrinkChoice = Random.Range(1, 3); //randomly choose choice of drink 
+        yield return new WaitForSeconds(OrderTime); //temporary way of making the customer wait for order
+        /*while (remainingtime < OrderTime) //will use while loop for actual waiting customer as these can be broken if the correct order is given
         {
             
             remainingtime += Time.deltaTime;
-            yield return null;
+            yield return new WaitForEndOfFrame();
         }*/
         SpeechBubble.SetActive(false);
-        while (remainingtime < 1f)
+        while (remainingtime < 1f) //same as the tothebar while loop lerp except in reverse as the customer is leaving the bar
         {
             transform.position = Vector3.Lerp(Endposition.position, Starposition.position, (remainingtime));
             remainingtime += Time.deltaTime;
             yield return null;
         }
-        
 
-
-
-
-
-
+        CustomerController.CustomerChoose = Random.Range(2,10); //randomizes value so that it will be either child or adult customer
+        CustomerController.choose = true;//allows the customercontroller update to be functional and can choose to activate child or adult
 
     }
 
--- Assets/Assignment/Scripts/CustomerController.cs
+++ Assets/Assignment/Scripts/CustomerController.cs
@@ -4,25 +4,60 @@ using UnityEngine;
 
 public class CustomerController : MonoBehaviour
 {
-    public bool IsSpawned;
-    public List<Customer> customers;
+    public static int CustomerChoose;
+
+    //public List<Customer> CustomerList;
+    public static Customer ChosenCustomer;
+    public Customer adultone;
+    public Customer childOne;
+    public static bool choose;
+    
+    public string PlayerDrinkChoice;
+    public string CustomerDrinkchoice;
     // Start is called before the first frame update
     void Start()
     {
-        IsSpawned = false;
+        choose = true;
+        CustomerChoose = 5;
+        //adultone.ToTheBar(Random.Range(0.5f, 3f));
+        
+    }
+
+    public static void ActivateCustomer()
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsSpawned == false)
+        /*if (IsSpawned == false)
         {
             Debug.Log("spawned");
             IsSpawned = true;
             
+        }*/
+
+        if (choose == false) return;
+        if (CustomerChoose <= 5)
+        {
+            Debug.Log(CustomerChoose);
+            //chosenOne. ;
+            adultone.StartBarTrip();
+ 
         }
-        
-        
+        if (CustomerChoose > 5)
+        {
+            Debug.Log(CustomerChoose);
+            childOne.StartBarTrip();
+        }
+
+    }
+
+    public void AdultChosen()
+    {
         
     }
+
+
 }
